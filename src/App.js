adf30fdcfcd72d8e032ed6a6e00845af5b6fb170
KIM <ehddnr0501@gmail.com>
2022-09-01 17:46:37 +0900

    Link tag 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
 import MainPageComponent from "./main/index.js";
+import UploadPage from "./upload";
+import ProductPage from "./product";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
-  return <MainPageComponent />;
+  return (
+    <dic>
+      <Switch>
+        <Route exact={true} path={"/"}>
+          <MainPageComponent />
+        </Route>
+        <Route exact={true} path={"/products/:id"}>
+          <ProductPage />
+        </Route>
+        <Route exact={true} path={"/upload"}>
+          <UploadPage />
+        </Route>
+      </Switch>
+    </dic>
+  );
 }
 
 export default App;
