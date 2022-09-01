b292ad8bcdce0e58ee2f3dc2b8acd86b0d76f314
KIM <ehddnr0501@gmail.com>
2022-09-01 17:02:25 +0900

    project init

---
+++ src/main/index.js
@@ -0,0 +1,62 @@
+import "./index.css";
+import axios from "axios";
+import React from "react";
+// return이 없을 경우, from을 뺌
+function MainPage() {
+  const [products, setProducts] = React.useState([]);
+  React.useEffect(function () {
+    axios
+      .get(
+        "https://b13064db-c9e1-4b08-a68f-4cf4979e74b1.mock.pstmn.io/products"
+      )
+      .then(function (result) {
+        const products = result.data.products;
+        setProducts(products);
+      })
+      .catch(function (error) {
+        console.error("error!!");
+      });
+  }, []);
+
+  return (
+    // 복수의 <div>를 return을 하려면 하나의 div로 씌워줘야 함
+    <div>
+      <div id="header">
+        <div id="header-area">
+          <img src="images/icons/logo.png" />
+        </div>
+      </div>
+      <div id="body">
+        <div id="banner">
+          <img src="images/banners/banner1.png" />
+        </div>
+        <h1>판매되는 상품들</h1>
+        <div id="products-list">
+          {products.map(function (product, index) {
+            return (
+              <div className="product-card">
+                <div>
+                  <img className="product-img" src={product.imageUrl} />
+                </div>
+                <div className="product-contents">
+                  <span className="product-name">{product.name}</span>
+                  <span className="product-price">{product.price}원</span>
+                  <div className="product-seller">
+                    <img
+                      className="product-avatar"
+                      src="images/icons/avatar.png"
+                    />
+                    <span>{product.seller}</span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div id="footer"></div>
+    </div>
+  );
+}
+
+export default MainPage;
