3d4720b2201827fbf26f9039a4f948abca1206be
KIM <ehddnr0501@gmail.com>
2022-09-05 15:59:08 +0900

    antd 기본 적용

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
+import "antd/dist/antd.css";
 import logo from "./logo.svg";
 import "./App.css";
 import MainPageComponent from "./main/index.js";
 import UploadPage from "./upload";
 import ProductPage from "./product";
-import { Switch, Route } from "react-router-dom";
-
+import { Switch, Route, Link, useHistory } from "react-router-dom";
+import { Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
 function App() {
+  const history = useHistory();
   return (
     <dic>
-      <Switch>
-        <Route exact={true} path={"/"}>
-          <MainPageComponent />
-        </Route>
-        <Route exact={true} path={"/products/:id"}>
-          <ProductPage />
-        </Route>
-        <Route exact={true} path={"/upload"}>
-          <UploadPage />
-        </Route>
-      </Switch>
+      <div id="header">
+        <div id="header-area">
+          <Link to="/">
+            <img src="/images/icons/logo.png" />
+          </Link>
+          <Button
+            size="large"
+            onClick={function () {
+              history.push("/upload");
+            }}
+            icon={<DownloadOutlined />}
+          >
+            상품 업로드
+          </Button>
+        </div>
+      </div>
+      <div id="body">
+        <Switch>
+          <Route exact={true} path={"/"}>
+            <MainPageComponent />
+          </Route>
+          <Route exact={true} path={"/products/:id"}>
+            <ProductPage />
+          </Route>
+          <Route exact={true} path={"/upload"}>
+            <UploadPage />
+          </Route>
+        </Switch>
+      </div>
+      <div id="footer"></div>
     </dic>
   );
 }
--- src/product/index.js
+++ src/product/index.js
@@ -1,8 +1,47 @@
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./index.css";
 
 function ProductPage() {
   const { id } = useParams();
-  return <h1>상품 상세 페이지 {id} 상품</h1>;
+  const [product, setProduct] = useState(null);
+  useEffect(function () {
+    axios
+      .get(
+        `https://b13064db-c9e1-4b08-a68f-4cf4979e74b1.mock.pstmn.io/products/${id}`
+      )
+      .then(function (result) {
+        setProduct(result.data);
+        // console.log(result);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+  // console.log(product);
+
+  if (product === null) {
+    return <h1>상품정보를 받고 있습니다...</h1>;
+  }
+
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageUrl} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+      <div id="contents-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}</div>
+        <div id="createdAt">2022년 9월 1일</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
